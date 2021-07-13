ac9dc50e1fc18709af9e7c36bb135e2543cb8834
pwu97 <43555461+pwu97@users.noreply.github.com>
2021-07-13 13:14:53 -0400

    Update DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,18 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+library(tidyverse)
+
+# Load Elsevier keywords
+elsevier_keywords <- read_csv("datasets/elsevier_keywords.csv")
+
+# Load UN SDG color scheme
+sdg_colors <- data.frame(
+  goal = c(1:17),
+  color = c('#e5243b', '#DDA63A', '#4C9F38', '#C5192D', '#FF3A21', '#26BDE2',
+            '#FCC30B', '#A21942', '#FD6925', '#DD1367', '#FD9D24', '#BF8B2E',
+            '#3F7E44', '#0A97D9', '#56C02B', '#00689D', '#19486A')
+)
+
+# Use datasets
+usethis::use_data(elsevier_keywords, overwrite = TRUE)
+usethis::use_data(sdg_colors, overwrite = TRUE)
