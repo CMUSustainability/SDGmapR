0f5889f3355fc18f1cd6e83a2438e42c8a7623e4
pwu97 <43555461+pwu97@users.noreply.github.com>
2021-07-20 14:30:30 -0400

    Add R documentation.

--- R/functions.R
+++ R/functions.R
@@ -10,6 +10,8 @@ library(tidyverse)
 #' @param keywords The specific data set from which to draw keywords (ex. "elsevier", "sdsn")
 #' @param count_repeats Whether or not to count repeats for the keywords
 #'
+#' @return A number representing the weight of words for an SDG
+#'
 #' @examples
 #' count_goal("Poverty entails more than the lack of income and productive resources
 #' to ensure sustainable livelihoods.", 1)
@@ -49,6 +51,20 @@ count_sdg_keywords <- Vectorize(function(text, goal_num, keywords="elsevier2",
     return (as.numeric(tot_weight))
 })
 
+#' @title Count SDGs keywords in a given text
+#'
+#' @description Determine the total weights of all SDGs keywords that exist in a given text
+#' for a given goal.
+#'
+#' @param text The target text to check SDGs keywords
+#' @param keywords The specific data set from which to draw keywords (ex. "elsevier", "sdsn")
+#' @param count_repeats Whether or not to count repeats for the keywords
+#'
+#' @return A number representing the weight of words for all SDGs
+#'
+#' @examples
+#' count_sdgs_keywords("Poverty entails more than the lack of income and productive resources
+#' to ensure sustainable livelihoods.")
 count_sdgs_keywords <- function(text, keywords="elsevier2",
                        count_repeats=FALSE) {
   tot_weight <- 0
@@ -58,3 +74,50 @@ count_sdgs_keywords <- function(text, keywords="elsevier2",
   }
   return (as.numeric(tot_weight))
 }
+
+################ Tabulate functions ################
+
+#' @title Tabulate SDG keywords in a given text
+#'
+#' @description Tabulate all SDG keywords that exist in a given text
+#' for a given goal.
+#'
+#' @param text The target text to check SDG keywords
+#' @param goal_num The SDG goal we intend to check keywords for
+#' @param keywords The specific data set from which to draw keywords (ex. "elsevier", "sdsn")
+#' @param count_repeats Whether or not to count repeats for the keywords
+#'
+#' @return A vector of keywords representing a particular SDG
+#'
+#' @examples
+#' tabulate_sdg_keywords("Poverty entails more than the lack of income and productive resources
+#' to ensure sustainable livelihoods.", 1)
+tabulate_sdg_keywords <- Vectorize(function(text, goal_num, keywords="elsevier2",
+                                         count_repeats=FALSE) {
+
+  # Select the right keyword set
+  if (keywords == "elsevier2") {
+    goal_df <- elsevier2_keywords %>%
+      filter(goal == goal_num)
+  } else if (keywords == "sdsn") {
+    goal_df <- sdsn_keywords %>%
+      filter(goal == goal_num)
+  } else if (keywords == "elsevier") {
+    goal_df <- elsevier_keywords %>%
+      filter(goal == goal_num)
+  }
+
+  # Get the keywords and weights
+  goal_keywords <- goal_df %>% select(keyword) %>% pull(1)
+  goal_weights <- goal_df %>% select(weight) %>% pull(1)
+
+  words <- c()
+  # Get keywords in a vector
+  for (idx in 1:nrow(goal_df)) {
+    if (str_detect(str_to_lower(text), goal_keywords[idx])) {
+      words <- c(words, goal_keywords[idx])
+    }
+  }
+
+  return (as.vector(words))
+})
